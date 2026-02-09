60df3c2443e1b24b3d5086af5f9a3a293d589fe8
Tyffoni <tyffoni@gmail.com>
2026-02-09 10:33:08 -0800

    added gender, discord name, and email in profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -27,12 +27,36 @@ export default function profile() {
           className="md:w-[158px] focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body  hover:bg-black/[.04] border border-gray-300"
           placeholder="Last Name"
         ></input>
-        <select name="Grade Level" id="Grade-Level">
+
+        <select
+          name="Grade Level"
+          id="Grade-Level"
+          className="md:w-[158px] focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body  hover:bg-black/[.04] border border-gray-300"
+        >
           <option value="fresh">Freshman</option>
           <option value="soph">Sophomore</option>
           <option value="junior">Junior</option>
           <option value="senior">Senior</option>
         </select>
+        <select
+          name="gender"
+          id="gender"
+          className="md:w-[158px] focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body  hover:bg-black/[.04] border border-gray-300"
+        >
+          <option value="fresh">Female</option>
+          <option value="soph">Male</option>
+          <option value="junior">Other</option>
+        </select>
+        <input
+          type="text"
+          className="md:w-[158px] focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body  hover:bg-black/[.04] border border-gray-300"
+          placeholder="Email"
+        ></input>
+        <input
+          type="text"
+          className="md:w-[158px] focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body  hover:bg-black/[.04] border border-gray-300"
+          placeholder="Discord Username"
+        ></input>
         <button className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]">
           Submit
         </button>
